fe85a13c6dfd6177e6cca4262a23128fca8a352d
chakaponden <IvanGovnov07@gmail.com>
2017-11-02 17:32:34 +0300

    refactoring HTMLParser

--- HTMLParser.cpp
+++ HTMLParser.cpp
@@ -25,21 +25,15 @@ std::vector<std::string> HTMLParser::Parse(const std::string& content)
     std::vector<std::string> fileRawReferences;
     EasyGumbo::Gumbo parser(content.c_str());
     EasyGumbo::Gumbo::iterator iter = parser.begin();
-    while(iter != parser.end())
+    // loop for each Dom node EasyGumbo::Element with link
+    while((iter =
+        std::find_if(iter, parser.end(),
+            And(EasyGumbo::Tag(GUMBO_TAG_LINK),
+                EasyGumbo::HasAttribute("href")))) != parser.end())
     {
-        iter = std::find_if(iter, parser.end(),
-                            And(EasyGumbo::Tag(GUMBO_TAG_LINK),
-                                EasyGumbo::HasAttribute("href")));
-        if (iter == parser.end())
-        {
-            break;
-        }
-        else
-        {
-            EasyGumbo::Element node(*iter);
-            fileRawReferences.push_back(std::string(node.attribute("href")->value));
-            ++iter;
-        }
+        // save every link value
+        EasyGumbo::Element node(*iter++);
+        fileRawReferences.push_back(std::string(node.attribute("href")->value));
     }
     return fileRawReferences;
 }
@@ -48,17 +42,18 @@ std::vector<std::string> HTMLParser::Parse(const std::string& content,
                                            const std::string& rootURL,
                                            const std::string& schemeURL)
 {
+    auto relativeURLToAbsoluteURL = [&rootURL, &schemeURL](const std::string& rawReference)
+    {
+        boost::network::uri::uri url;
+        url << boost::network::uri::scheme(schemeURL)
+            << boost::network::uri::host(rootURL)
+            << boost::network::uri::path(rawReference);
+        return url.string();    
+    };
     std::vector<std::string> fileRawReferences = Parse(content);
     std::vector<std::string> fileFullReferences;
     std::transform(fileRawReferences.begin(), fileRawReferences.end(),
                    std::back_inserter(fileFullReferences),
-                   [&rootURL, &schemeURL](const std::string& rawReference)
-                   {
-                        boost::network::uri::uri url;
-                        url << boost::network::uri::scheme(schemeURL)
-                            << boost::network::uri::host(rootURL)
-                            << boost::network::uri::path(rawReference);
-                        return url.string();
-                    });
+                   relativeURLToAbsoluteURL);
     return fileFullReferences;
 }
