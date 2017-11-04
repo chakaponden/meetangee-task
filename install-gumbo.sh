21fe9e492a5bbea52f4e52fa27f4b910cf87e2da
chakaponden <IvanGovnov07@gmail.com>
2017-11-04 13:36:16 +0300

    update travis and install gumbo script

--- install-gumbo.sh
+++ install-gumbo.sh
@@ -1,10 +1,11 @@
 # bash
 # install google gumbo parcer from git sources
+rm ./gumbo-parser -R -f
 git clone https://github.com/google/gumbo-parser
 cd ./gumbo-parser
-./autogen.sh
-./configure
-make
-sudo make install
+./autogen.sh --quiet
+./configure --quiet
+make --silent
+sudo make install --silent
 cd ..
 rm ./gumbo-parser -R
